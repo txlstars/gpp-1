0e53f840fc01cf440d4837f9f208e40f7d7ec376
txlstars <txlstars@gmail.com>
2019-06-20 20:03:56 -0400

    commit

--- tools/sql/gpp.sql
+++ tools/sql/gpp.sql
@@ -7,10 +7,10 @@ create table t_doc_info (
 	docid varchar(128) not null,
 	title varchar(256) not null, 
 	src varchar(32) not null,
-	type varchar(32);
+	type varchar(32),
 	views int default 0,
 	loves int default 0,
-	publish_time timestamp default '1970-01-01 00:00:00';
+	publish_time timestamp default '1970-01-01 00:00:00',
 	create_time timestamp not null default current_timestamp,
 	update_time timestamp not null default current_timestamp on update current_timestamp,
 	primary key(id) );
