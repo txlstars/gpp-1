04da00aa79d081f583e434e18215bee583ff19c5
txlstars <txlstars@gmail.com>
2019-06-23 12:58:44 -0400

    commit

--- tools/sql/gpp.sql
+++ tools/sql/gpp.sql
@@ -6,8 +6,10 @@ create table t_doc_info (
 	id bigint auto_increment,
 	docid varchar(128) not null,
 	title varchar(256) not null, 
+	summary varchar(512) default '',
 	src varchar(32) not null,
 	type varchar(32),
+	pageurl varchar(256) not null,
 	publish_time timestamp default '1970-01-01 00:00:00',
 	create_time timestamp not null default current_timestamp,
 	update_time timestamp not null default current_timestamp on update current_timestamp,
@@ -16,15 +18,13 @@ create table t_doc_info (
 ) default charset = utf8;
 
 # 创建doc动态信息表
-create table t_doc_info (
+create table t_doc_dynamic_info (
 	id bigint auto_increment,
 	docid varchar(128) not null,
 	src varchar(32) not null,
 	views int default 0,
 	loves int default 0,
-	publish_time timestamp default '1970-01-01 00:00:00',
-	create_time timestamp not null default current_timestamp,
 	update_time timestamp not null default current_timestamp on update current_timestamp,
 	primary key(id),
 	constraint uqdocid unique(src, docid);
-);
+) default charset = utf8;
